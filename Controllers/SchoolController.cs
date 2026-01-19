fad5b0f9970f6e9e2fa1cecb99e5304a1457c52f
Sayan0361 <sayansen0361@gmail.com>
2026-01-19 15:46:04 +0530

    Added linked to existing schools with Site

---
+++ Controllers/SchoolController.cs
@@ -0,0 +1,49 @@
+﻿using MiniReportsProject.DAL;
+using MiniReportsProject.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MiniReportsProject.Controllers
+{
+    public class SchoolController : Controller
+    {
+        SchoolDAL _schoolDAL = new SchoolDAL();
+
+        // GET: School
+        // single Index action with optional site id to avoid ambiguous matches
+        public ActionResult Index(int? id)
+        {
+            // if caller provided a site id, expose it to the view (used for linking)
+            if (id.HasValue)
+            {
+                ViewBag.SiteId = id.Value;
+            }
+
+            // currently GetAllSchoolNames returns the same list in both cases;
+            // change DAL if you need different behaviour when id is present (e.g. only unlinked schools)
+            List<SchoolModel> schools = _schoolDAL.GetAllSchoolNames();
+            return View(schools);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LinkSelected(int siteId, int[] selectedIds)
+        {
+            if (selectedIds == null || selectedIds.Length == 0)
+            {
+                TempData["Error"] = "Please select at least one school.";
+                return RedirectToAction("Index", new { id = siteId });
+            }
+
+            int linkedCount = 0;
+
+            foreach (var schoolId in selectedIds)
+            {
+                linkedCount += _schoolDAL.LinkSchoolToSite(schoolId, siteId);
+            }
+
+            TempData["Success"] = $"{linkedCount} school(s) linked successfully.";
+            return RedirectToAction("Index", "Site", new { id = siteId });
+        }
+    }
+}
---
+++ DAL/SchoolDAL.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using MiniReportsProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace MiniReportsProject.DAL
+{
+    public class SchoolDAL
+    {
+        public List<SchoolModel> GetAllSchoolNames()
+        {
+            try
+            {
+                using (var db = DapperContext.GetConnection())
+                {
+                    return db.Query<SchoolModel>(
+                        "sp_GetAllSchoolNames",
+                        commandType: CommandType.StoredProcedure
+                    ).ToList();
+                }
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Fetching Grantee Types Failed in catch block: " + err.Message);
+            }
+        }
+
+        public int LinkSchoolToSite(int schoolId, int siteId)
+        {
+            try
+            {
+                using (var db = DapperContext.GetConnection())
+                {
+                    return db.Execute(
+                        "sp_LinkSchoolToSite",
+                        new { SchoolID = schoolId, SiteID = siteId },
+                        commandType: CommandType.StoredProcedure
+                    );
+                }
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Fetching Grantee Types Failed in catch block: " + err.Message);
+            }
+        }
+    }
+}
