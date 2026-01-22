84a3b8dcd4375627d8aa9353c6eae5b7a628493a
Subhradeep <subhradeepbasu2002@gmail.com>
2026-01-22 15:56:42 +0530

    View Program with Linked sites and schools

--- Controllers/GranteeController.cs
+++ Controllers/GranteeController.cs
@@ -11,17 +11,23 @@ namespace MiniReportsProject.Controllers
     public class GranteeController : Controller
     {
         GranteeDAL _granteeDAL = new GranteeDAL();
+        ProgramDAL _programDAL = new ProgramDAL();
         // GET: Grantee
         public ActionResult Index(int ID)
         {
             var SiteList = _granteeDAL.GetAllSitesByGranteeID(ID);
             string GranteeName = _granteeDAL.GetGranteeNameByID(ID);
+
+            var ProgramData = _programDAL.GetProgramData();
+
             int GranteeID = ID;
 
             var siteViewModel = new SiteViewModel();
             siteViewModel.siteList = SiteList;
             siteViewModel.GranteeName = GranteeName;
             siteViewModel.GranteeID = GranteeID;
+
+            siteViewModel.ProgramList = ProgramData;
             // pass the string as the model (not as a view name)
             //return View((object)GranteeName);
             return View(siteViewModel);
